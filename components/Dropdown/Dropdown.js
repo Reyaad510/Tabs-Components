6fa82b43e9d44007e7b2e694b80a525123c2afed
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-07 20:31:42 -0500

    Removed '.' from the classList and now menu is working

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -3,14 +3,16 @@ class Dropdown {
     
     // Assign this.element to the dropdown element
     this.element = element;
-    
+    console.log(element);
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-    console.log(button);
+    console.log(this.button);
+    
     
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.dropdown-content');
+    console.log(this.content);
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
@@ -23,12 +25,14 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('.dropdown-hidden');
-  
+    this.content.classList.toggle('dropdown-hidden');
+    
+    
   }
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
 
